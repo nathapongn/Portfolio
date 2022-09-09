fc2ada1f42637b6358ae097cfa5873e7f29c0824
Nathapong <101572359+nathapongn@users.noreply.github.com>
2022-09-10 00:33:24 +0700

    Create

---
+++ script.js
@@ -0,0 +1,14 @@
+const ham = document.querySelector(".ham");
+const navmenu = document.querySelector(".navmenu");
+
+ham.addEventListener("click", () => {
+    ham.classList.toggle("active");
+    navmenu.classList.toggle("active");
+})
+
+var typed = new Typed(".auto-type", {
+    strings: ["Nat", "a UX/UI Designer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
