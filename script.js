922e16bc5a6355f85fe095853dbbd6ede569698e
Nathapong <101572359+nathapongn@users.noreply.github.com>
2024-01-31 23:42:07 +0700

    update intro cover

--- script.js
+++ script.js
@@ -24,13 +24,3 @@ function scrollr(){
 
 
 
-// progress bar
-
-function updateProgressBar(){
-    const {scrollTop, scrollHeight} = document.documentElement;
-    const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100 + '%';
-    console.log(scrollPercent);
-    document.querySelector('.progress-bar').style.setProperty('--progress', scrollPercent);
-}
-
-document.addEventListener('scroll', updateProgressBar)
